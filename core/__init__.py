2f663857a1fd3d3ae81dd9cafe6e77ce7669e801
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-09-27 21:09:16 +0530

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,15 @@
+from telethon import TelegramClient
+from decouple import config
+import logging
+import time
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# Basics
+APP_ID = config("APP_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SUDO_USERS = config("SUDO_USERS", default=None)
+
+zbot = TelegramClient('kk', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
